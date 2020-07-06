dbffb964c8e976093bce12a06ff0706034d02140
yangzhilong05 <yangzhilong05@countrygarden.com.cn>
2020-07-06 19:22:34 +0800

    add

---
+++ service-manage/src/test/java/com/service/feign/servicemanage/ServiceManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.feign.servicemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
